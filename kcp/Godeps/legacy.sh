b8778363998f285080409cbac5945c9675699ac7
fananchong <fananchong@126.com>
2018-07-27 18:22:20 +0800

    add vendor (kcp-go)

--- kcp/Godeps/legacy.sh
+++ kcp/Godeps/legacy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker run --rm -v /d/temp/:/temp/ -e GOPATH=/temp golang go get -u -d github.com/xtaci/kcp-go
+set -ex
+
+git config http.proxy http://127.0.0.1:8123
+git config https.proxy https://127.0.0.1:8123
+
+docker run --rm --net=host -v /d/temp/:/temp/ -e GOPATH=/temp golang go get -u -d -insecure github.com/xtaci/kcp-go
+
+git config --unset http.proxy
+git config --unset https.proxy
+
