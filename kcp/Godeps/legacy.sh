3daf289699efd4bc51f3b3e53971163dfe72ff64
fananchong <fananchong@126.com>
2018-07-31 18:26:32 +0800

    up

--- kcp/Godeps/legacy.sh
+++ kcp/Godeps/legacy.sh
@@ -2,11 +2,5 @@
 
 set -ex
 
-git config http.proxy http://127.0.0.1:8123
-git config https.proxy https://127.0.0.1:8123
-
-docker run --rm --net=host -v /d/temp/:/temp/ -e GOPATH=/temp golang go get -u -d -insecure github.com/xtaci/kcp-go
-
-git config --unset http.proxy
-git config --unset https.proxy
+docker run --rm --net=host -v /d/temp/:/temp/ -e GOPATH=/temp golang /bin/bash -c "git config --global http.proxy http://127.0.0.1:8123 && git config --global https.proxy https://127.0.0.1:8123 && go get -u -d -insecure github.com/xtaci/kcp-go"
 
