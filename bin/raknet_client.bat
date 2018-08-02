10f1d46a689c447e5517053260dfe6228bce7573
fananchong <fananchong@126.com>
2018-08-02 18:44:56 +0800

    fix raknet server bind ip

---
+++ bin/raknet_client.bat
@@ -0,0 +1,2 @@
+cd x64-Release\client\RelWithDebInfo
+client.exe 192.168.93.183
--- raknet/server/server_raknet.cpp
+++ raknet/server/server_raknet.cpp
@@ -40,6 +40,7 @@ bool StartRakNetServer(int port, int broadcastInterval, const char* broadcastMsg
 	RakNet::SocketDescriptor socketDescriptors[1];
 	socketDescriptors[0].port = atoi(portstring);
 	socketDescriptors[0].socketFamily = AF_INET; // Test out IPV4
+	sprintf(socketDescriptors[0].hostAddress, "%s", "0.0.0.0");
 	bool b = server->Startup(8, socketDescriptors, 1) == RakNet::RAKNET_STARTED;
 	if (!b)
 	{
